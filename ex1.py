60ab3726a609527e75a51a2c0308c86378d09512
alex <alex.fql@gmail.com>
2015-10-07 19:09:58 +0800

    练习

--- ex1.py
+++ ex1.py
@@ -5,14 +5,3 @@ print "This is fun."
 print 'Yay!Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
-
-# A comment, this is so you can read your program later.
-# Anything after the # is ignored by python.
-
-print "I could have code like this." # and the comment after is ignored
-# You can also use a comment to "disable" or comment out a piece of code:
-# print "This won't run."
-
-print "This will run."
-
-print 1+1
